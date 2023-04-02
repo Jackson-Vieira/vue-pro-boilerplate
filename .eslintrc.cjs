86c845aa24d0f13c5b8aaaf6035aa6620b8d6eb5
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-04-02 13:59:20 -0300

    define base configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,30 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+
+  rules: {
+    // regra para nomes de componentes em PascalCase
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: []
+      }
+    ],
+    // regra para ordem de atributos em tags
+    'vue/attributes-order': 'error',
+    // regra para o uso de aspas em atributos
+    'vue/html-quotes': ['error', 'double'],
+    // regra para espaçamento em tags com fechamento vazio
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always'
+      }
+    ]
   }
 }
