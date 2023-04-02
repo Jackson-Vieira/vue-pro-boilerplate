7fd32dc720d5d53c495bd10b5e477921a0a6dfd2
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-04-02 14:00:46 -0300

    remove vue project start components

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<template>
+  <h1>
+    Hello, World
+  </h1>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <HelloWorld />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
